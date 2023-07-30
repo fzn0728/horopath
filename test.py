71afea9bba7e94f0d913d9c69640c12a0c872af1
Renee <reneehuang@Renees-MacBook-Pro.local>
2023-07-29 18:50:39 -0700

    cmt

--- test.py
+++ test.py
@@ -7,9 +7,6 @@ load_dotenv() # load environment variables from .env file
 anthropic = Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"),)
 
 def main():
-    # Here you need to define what Query is before using it
-    # For instance, if it is a class you have defined, you might need to create an instance of it
-    # Query = YourClass()
     horoscope = get_horoscope(Query)
     astro_tbl = extract_astro_table(horoscope)
     aspect_tbl = extract_aspects(horoscope)
